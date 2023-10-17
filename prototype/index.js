30732a1126361fa0efe31a0ff1fbe8f0cd30725b
julia-moran <juliamoran1001@gmail.com>
2023-10-16 21:26:29 -0400

    Change port and host

--- prototype/index.js
+++ prototype/index.js
@@ -79,7 +79,7 @@ io.on('connection', async (socket) => {
   }
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, '0.0.0.0', () => {
     console.log('server running at http;//localhost:3000');
 });
 }
