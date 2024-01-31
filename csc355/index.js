44878eb545868f87d441a1b5a7d87d4e13f34496
julia-moran <juliamoran1001@gmail.com>
2024-01-31 16:10:11 -0500

    Add password checker and draft for database connection

--- csc355/index.js
+++ csc355/index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const path = require('path');
 
+
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
 app.get('/create-account', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'create-account.html'));
   });
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
--- csc355/public/createAccount.js
+++ csc355/public/createAccount.js
@@ -1,8 +1,26 @@
+//import { Pool, Client } from 'pg';
+
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
 const createAccount = document.getElementById('createAccount');
 const errorMessage = document.getElementById('errorMessage');
+//import * as db from '../db.js'
 
+/*
+app.get('/:id', async (req, res, next) => {
+  const result = await db.query('SELECT * FROM users WHERE id = $1', [req.params.id])
+  res.send(result.rows[0])
+})
+
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "21184",
+    database: "postgres"
+})
+*/
 
 
 createAccount.addEventListener('submit', (e) => {
@@ -13,7 +31,19 @@ createAccount.addEventListener('submit', (e) => {
         errorMessage.innerHTML = "Password must be at least seven characters";
     } else if (!(password.value.match(/\d/g))) {
         errorMessage.innerHTML = "Password must contain at least one numeral";
+    } else if (!(password.value.match(/[a-zA-Z]/g))){
+        errorMessage.innerHTML = "Password must contain at least one letter";
     } else {
         location.replace("/");
     }
+
+    /*client.connect((err) => {
+        if (err) throw err;
+  
+        client.query('SELECT * FROM users', (err, res) => {
+          console.log(err ? err.stack : res.rows)
+          client.end()
+        })
+      });
+*/
 });
