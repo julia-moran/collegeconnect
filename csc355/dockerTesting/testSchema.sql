3ffdf6d45f6520b38bbb198ca7e32f2c3d50abd0
jhami311 <jhami311@live.kutztown.edu>
2024-02-21 18:44:56 -0500

    superuser fix

--- csc355/dockerTesting/testSchema.sql
+++ csc355/dockerTesting/testSchema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE userInfo (
-    userID INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     displayName VARCHAR(32),
     email VARCHAR(50),
     clearance BOOLEAN CHECK (clearance::integer = 0 OR clearance::integer = 1),
@@ -44,18 +44,18 @@ CREATE TABLE chatLog (
     classSection VARCHAR(10)
 );
 
-INSERT INTO userInfo(userid,displayname, email, clearance,firstname,lastname, major,minor) 
+INSERT INTO userInfo(displayname, email, clearance,firstname,lastname, major,minor) 
 VALUES
-('32412536','jmora678','jmora678@live.kutztown.edu','0','Julia','Moran','Computer Science','Mathmatics'),
-('32412436','jhami311','jhami311@live.kutztown.edu','0','Jack', 'Hamilton','Information Technology', ' '),
-('32412336','tkasp445','tkasp445@live.kutztown.edu','1','Thomas','Kasper','Information Technology', ' '),
-('32412335','jserr892','jserr892@live.kutztown.edu','1','Jerome','Serrao', 'Information Technology', ' '),
-('32412111','lblac213','lblac213@live.kutztown.edu','1','Lon','Blackwell','Computer Science','Physics'),
-('32412862','jruss324','jruss324@live.kutztown.edu','1','Janine','Russell','Computer Science','Biology'),
-('32413590','jpace578','jpace578@live.kutztown.edu','1','Jeremiah','Pace', 'Game Development','Cinema,Television, and Media'),
-('32412278','nhoga231','nhoga231@live.kutztown.edu','1','Neal', 'Hogan', 'Information Technology','Criminal Justice'),
-('32417363','mmatt325','mmatt325@live.kutztown.edu','1','Martin','Matthews','Game Development',''),
-('32419012', 'smars432', 'smars432@live.kutztown.edu','1','Sandra','Marsh','Information Technology','English');
+('jmora678','jmora678@live.kutztown.edu','0','Julia','Moran','Computer Science','Mathmatics'),
+('jhami311','jhami311@live.kutztown.edu','0','Jack', 'Hamilton','Information Technology', ' '),
+('tkasp445','tkasp445@live.kutztown.edu','1','Thomas','Kasper','Information Technology', ' '),
+('jserr892','jserr892@live.kutztown.edu','1','Jerome','Serrao', 'Information Technology', ' '),
+('lblac213','lblac213@live.kutztown.edu','1','Lon','Blackwell','Computer Science','Physics'),
+('jruss324','jruss324@live.kutztown.edu','1','Janine','Russell','Computer Science','Biology'),
+('jpace578','jpace578@live.kutztown.edu','1','Jeremiah','Pace', 'Game Development','Cinema,Television, and Media'),
+('nhoga231','nhoga231@live.kutztown.edu','1','Neal', 'Hogan', 'Information Technology','Criminal Justice'),
+('mmatt325','mmatt325@live.kutztown.edu','1','Martin','Matthews','Game Development',''),
+('smars432', 'smars432@live.kutztown.edu','1','Sandra','Marsh','Information Technology','English');
 
 INSERT INTO classList (roomNum, userID, displayName, email)
 VALUES
