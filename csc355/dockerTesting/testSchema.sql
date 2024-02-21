77695b77ef904678cc01401104bd7d937136d88e
jhami311 <jhami311@live.kutztown.edu>
2024-02-21 17:46:26 -0500

    db working in container

--- csc355/dockerTesting/testSchema.sql
+++ csc355/dockerTesting/testSchema.sql
@@ -43,3 +43,46 @@ CREATE TABLE chatLog (
     className VARCHAR(50),
     classSection VARCHAR(10)
 );
+
+INSERT INTO userInfo(userid,displayname, email, clearance,firstname,lastname, major,minor) 
+VALUES
+('32412536','jmora678','jmora678@live.kutztown.edu','0','Julia','Moran','Computer Science','Mathmatics'),
+('32412436','jhami311','jhami311@live.kutztown.edu','0','Jack', 'Hamilton','Information Technology', ' '),
+('32412336','tkasp445','tkasp445@live.kutztown.edu','1','Thomas','Kasper','Information Technology', ' '),
+('32412335','jserr892','jserr892@live.kutztown.edu','1','Jerome','Serrao', 'Information Technology', ' '),
+('32412111','lblac213','lblac213@live.kutztown.edu','1','Lon','Blackwell','Computer Science','Physics'),
+('32412862','jruss324','jruss324@live.kutztown.edu','1','Janine','Russell','Computer Science','Biology'),
+('32413590','jpace578','jpace578@live.kutztown.edu','1','Jeremiah','Pace', 'Game Development','Cinema,Television, and Media'),
+('32412278','nhoga231','nhoga231@live.kutztown.edu','1','Neal', 'Hogan', 'Information Technology','Criminal Justice'),
+('32417363','mmatt325','mmatt325@live.kutztown.edu','1','Martin','Matthews','Game Development',''),
+('32419012', 'smars432', 'smars432@live.kutztown.edu','1','Sandra','Marsh','Information Technology','English');
+
+INSERT INTO classList (roomNum, userID, displayName, email)
+VALUES
+('OM287', 32412536, 'jmora678', 'jmora678@live.kutztown.edu'),
+('OM287', 32412436, 'jhami311', 'jhami311@live.kutztown.edu'),
+('OM287', 32412336, 'tkasp445', 'tkasp445@live.kutztown.edu'),
+('OM287', 32412111, 'lblac213', 'lblac213@live.kutztown.edu'),
+('OM287', 32412862, 'jruss324', 'jruss324@live.kutztown.edu'),
+('OM287', 32413590, 'jpace578', 'jpace578@live.kutztown.edu'),
+('OM287', 32412278, 'nhoga231', 'nhoga231@live.kutztown.edu'),
+('OM287', 32417363, 'mmatt325', 'mmatt325@live.kutztown.edu'),
+('OM287', 32419012, 'smars432', 'smars432@live.kutztown.edu');
+
+INSERT INTO chatroom(roomnum,classcode,classname,classsection,classroom,classprof)
+VALUES
+(1,'CSC101','Computer Applications','S810','Old Main 159', 'Jici Huang'),
+(2,'CSC111','Computer Forensics','S010','De Francesco 103', 'Griffin Nye'),
+(3,'CSC135','Computer Science I','S030','Grim Building 307','Hallie Langley'),
+(4,'CSC223','Advanced Scientific Programming','S010','Old Main 158', 'John Carelli'),
+(5,'CSC220','Object-oriented Programming','S020','Old Main 159','Dylan Schwesinger'),
+(6,'CSC242','Server-side Web Development','S010','Old Main 158','John Carelli'),
+(7,'CSC310','Programming Languages','S010','Old Main 159','Yong-Sang Shim'),
+(8,'CSC330','Introduction to Mobile Architecture and Systems','S010','Old Main 158','Jianfeng Wang'),
+(9,'CSC355', 'Software Engineering II','S811','Old Main 287','Dylan Schwesinger'),
+(10,'CSC402','Data Structures II','S101','Old Main 158','Yong Zhang'),
+(11,'CSC458','Data Mining and Predictive Analytics I','S401','Old Main 158','Dale Parson'),
+(12,'CSC447','Artificial Intelligence I','S900','Online','Dylan Schwesinger'),
+(13,'CSC543','Multiprocessing and Concurrent Programing','S201','Old Main 158','Dale Parson'),
+(14,'CSC510','Advanced Operating Systems','S301','Old Main 158','Dylan Schwesinger'),
+(15,'CSC555','Applied Cryptography','S501','Old Main 158','Yong Zhang');
