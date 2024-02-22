bcde2c96ad2326b6d614eec1f5433aa7a9fc0a9a
jhami311 <jhami311@live.kutztown.edu>
2024-02-22 15:01:45 -0500

    compose

--- csc355/dockerTesting/index.js
+++ csc355/dockerTesting/index.js
@@ -1,16 +1,23 @@
 const { Client } = require('pg');
 
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'collegeconnect',
-  port: 5432,
-});
+const connectWithRetry = () => {
+  const client = new Client({
+    user: 'postgres',
+    host: 'postgres',
+    database: 'collegeconnect',
+    password: '0285',
+    port: 5432,
+  });
 
-client.connect(err => {
-  if (err) {
-    console.error('connection error', err.stack);
-  } else {
-    console.log('connected');
-  }
-});
+  return client.connect((err) => {
+    if (err) {
+      console.error('Failed to connect to postgres on startup - retrying in 5 sec', err);
+      client.end();
+      setTimeout(connectWithRetry, 5000);
+    } else {
+      console.log('Successfully connected to postgres');
+    }
+  });
+};
+
+connectWithRetry();
