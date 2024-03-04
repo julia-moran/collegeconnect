66a91aa8e3bd76fcbc491ad319ec48db32742476
jhami311 <jhami311@live.kutztown.edu>
2024-03-04 09:21:04 -0500

    Update login.js

--- csc355/build/login.js
+++ csc355/build/login.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
                 if (data.message === 'Login successful!') {
 // save userID and print (testing purposes only, will be removed later)
                     sessionStorage.setItem("currentID", data.id);
-                    alert('Login successful! You will be redirected to the homepage. User ID: ' + sessionStorage.getItem("currentID"));
+                    alert('Login successful! You will be redirected to your profile. User ID: ' + sessionStorage.getItem("currentID"));
         
                     window.location.replace("../profile-view");
                 }
