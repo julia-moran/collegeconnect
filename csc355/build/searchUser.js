818112fd54e83d3bfdde79cc8d38059ef8df0655
julia-moran <juliamoran1001@gmail.com>
2024-03-21 21:14:57 -0400

    Add filter name

--- csc355/build/searchUser.js
+++ csc355/build/searchUser.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     if (sessionStorage.length == 0) {
          window.location.replace("../");
     } else {
+        $("#filter").hide();
          $('.select-multiple').select2();
 
         $.post('/displayClasses', { id: sessionStorage.getItem("currentID") },
@@ -31,15 +32,59 @@ $(document).ready(function() {
             })
         });
 
-        const form = document.querySelector('form');
+        const form = document.getElementById('searchForm');
         const table = document.querySelector('table');
+        const filterForm = document.getElementById('filterForm');
+        let searchResults = [];
 
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-
+        function populateResults (searchResults) {
             while(table.firstChild) {
                 table.removeChild(table.firstChild);
             }
+
+            for(let i in searchResults) {
+                console.log(searchResults[i]);
+                const tableRow = document.createElement('tr');
+                tableRow.id = "result" + searchResults[i].id;
+                const name = document.createElement('td')
+                table.appendChild(tableRow);
+                name.textContent = searchResults[i].fname + " " + searchResults[i].lname;
+                tableRow.appendChild(name);
+                const major = document.createElement('td')
+                major.textContent = "Major: " + searchResults[i].major;
+                tableRow.appendChild(major);
+                const minor = document.createElement('td')
+                minor.textContent = "Minor: " + searchResults[i].minor;
+                tableRow.appendChild(minor);
+                const sharedClass = document.createElement('td')
+                sharedClass.textContent = "Shares ";
+                $.post('/searchForSharedClasses', { id: sessionStorage.getItem("currentID"), searchedUserID: searchResults[i].id },
+                function(classResults, status) {
+                    $(classResults).each(function(i, classResult) {
+                        sharedClass.textContent = sharedClass.textContent + classResult.classcode + " ";
+                        tableRow.appendChild(sharedClass);
+                     });
+            
+                });
+            }
+        }
+
+        filterForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            if($("#filterlname").val()) {
+                searchResults = searchResults.filter((result) => result.lname == $("#filterlname").val());
+            }
+
+            if($("#filterfname").val()) {
+                searchResults = searchResults.filter((result) => result.fname == $("#filterfname").val());
+            }
+            
+            populateResults(searchResults);
+        });
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
         
             $.post('/searchUsers', {
                 id: sessionStorage.getItem("currentID"),
@@ -48,30 +93,22 @@ $(document).ready(function() {
                 major: $("#selectMajor option:selected").text(),
                 minor: $("#selectMinor option:selected").text()
             }, function(results, status) {
+                if(results) {
+                    $("#filter").show();
+                }
                 $(results).each(function(i, result) {
-                    const tableRow = document.createElement('tr');
-                    tableRow.id = "result" + result.id;
-                    const name = document.createElement('td')
-                    table.appendChild(tableRow);
-                    name.textContent = result.firstname + " " + result.lastname;
-                    tableRow.appendChild(name);
-                    const major = document.createElement('td')
-                    major.textContent = "Major: " + result.major;
-                    tableRow.appendChild(major);
-                    const minor = document.createElement('td')
-                    minor.textContent = "Minor: " + result.minor;
-                    tableRow.appendChild(minor);
-                    const sharedClass = document.createElement('td')
-                    sharedClass.textContent = "Shares ";
-                    $.post('/searchForSharedClasses', { id: sessionStorage.getItem("currentID"), searchedUserID: result.id },
-                    function(classResults, status) {
-                        $(classResults).each(function(i, classResult) {
-                            sharedClass.textContent = sharedClass.textContent + classResult.classcode + " ";
-                            tableRow.appendChild(sharedClass);
+                    searchResults.push({
+                        id: result.id,
+                        fname: result.firstname,
+                        lname: result.lastname,
+                        major: result.major,
+                        minor: result.minor
                     });
-                });
+                })
+
+                populateResults(searchResults);
             });
-            });
+        
 
         }); 
     }
