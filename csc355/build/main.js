4c1927b64a0e1b4c995d06be1756b204b2f09c77
jhami311 <jhami311@live.kutztown.edu>
2024-04-16 12:21:31 -0400

    Update main.js

--- csc355/build/main.js
+++ csc355/build/main.js
@@ -1,9 +1,9 @@
 window.onload = function() {
     var isLoggedIn = sessionStorage.getItem("currentID") !== null;
-    console.log('User logged in:', isLoggedIn); // Log whether the user is logged in
+    console.log('User logged in:', isLoggedIn);
   
     var clearance = sessionStorage.getItem("clearance");
-    console.log('User clearance:', clearance); // Log the user's clearance
+    console.log('User clearance:', clearance);
   
     var buttonsToHideWhenLoggedOut = [
         "/chatRoom",
@@ -16,18 +16,15 @@ window.onload = function() {
   
     var buttons = document.querySelectorAll("nav button");
 
-    // Check if there is a page stored in the sessionStorage
     var lastPage = sessionStorage.getItem("lastPage");
     if (lastPage) {
-        // Navigate to the last page
         navigateTo(lastPage);
     }
 }
   
 function navigateTo(page) {
-    console.log('Navigating to page:', page); // Log the page being navigated to
+    console.log('Navigating to page:', page);
     document.getElementById('mainFrame').src = page;
 
-    // Store the current page in the sessionStorage
     sessionStorage.setItem("lastPage", page);
 }
