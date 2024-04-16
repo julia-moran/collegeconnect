c6236b91ac475cd905016e33ec4281bf18944fd5
jhami311 <jhami311@live.kutztown.edu>
2024-04-16 12:19:38 -0400

    Update main.js

--- csc355/build/main.js
+++ csc355/build/main.js
@@ -16,9 +16,18 @@ window.onload = function() {
   
     var buttons = document.querySelectorAll("nav button");
 
-  }
+    // Check if there is a page stored in the sessionStorage
+    var lastPage = sessionStorage.getItem("lastPage");
+    if (lastPage) {
+        // Navigate to the last page
+        navigateTo(lastPage);
+    }
+}
   
-  function navigateTo(page) {
-      console.log('Navigating to page:', page); // Log the page being navigated to
-      document.getElementById('mainFrame').src = page;
-  }
+function navigateTo(page) {
+    console.log('Navigating to page:', page); // Log the page being navigated to
+    document.getElementById('mainFrame').src = page;
+
+    // Store the current page in the sessionStorage
+    sessionStorage.setItem("lastPage", page);
+}
